0eda2a1551278dc6872b921415e4e6f27cf0337e
DenisVorop <saskeazaske@icloud.com>
2022-05-31 20:54:00 +0300

    search episode in seasons done

--- src/components/Season/Season.tsx
+++ src/components/Season/Season.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import { TEpisode } from '../../types/types';
+
 import EpisodeRow from '../EpisodeRow/EpisodeRow';
+
+import useInput from '../../hooks/useInput';
 
 import './season.scss'
 
@@ -10,28 +14,32 @@ interface SeasonProps {
 }
 
 const Season: React.FC<SeasonProps> = ({ season, episodes }) => {
+    const { value: seasonSearchValue, bind: seasonSearchBind } = useInput('')
+
     return (
         <div className='season__wrapper'>
             <div className="season__top">
                 <div className="season__title">{season} Season</div>
                 <div className="season__search">
-                    <input type="text" placeholder='Search episode' />
+                    <input type="text" placeholder='Search episode' {...seasonSearchBind} />
                 </div>
                 <div className="season__sort">
                     <input type="text" placeholder='sort by' />
                 </div>
             </div>
             <div className="season__body">
-                {episodes.map((episode: TEpisode) => {
-                    if (episode.episode.substring(2, 3) === season)
-                        return (
-                            <EpisodeRow
-                                key={episode.name}
-                                episode={episode}
-                            />
-                        )
-                    return null
-                })}
+                {episodes
+                    .filter((episode: TEpisode) => episode.name.toLowerCase().includes(seasonSearchValue.toLowerCase()))
+                    .map((episode: TEpisode) => {
+                        if (episode.episode.substring(2, 3) === season)
+                            return (
+                                <EpisodeRow
+                                    key={episode.name}
+                                    episode={episode}
+                                />
+                            )
+                        return null
+                    })}
             </div>
         </div>
     )
